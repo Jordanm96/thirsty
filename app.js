aef0ac338b9776a9910758f0dd4ba21cce2abcd6
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-28 14:13:35 -0700

    Added ternary for null ingredient values

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ async function fetchData() {
     let response = await axios.get(url)
     let data = response.data.drinks[0]
     showCocktailData(data)
+    console.log(data)
   } catch (error) {
     console.error(error)
   }
@@ -15,15 +16,22 @@ function showCocktailData(data) {
   const cocktailInfo = `
   <h2>${data.strDrink}</h2>
   <img src="${data.strDrinkThumb}" alt="drink"/>
-  <ul>Ingredients: 
-  <li>${data.strMeasure1} ${data.strIngredient1}</li>
-  <li>${data.strMeasure2} ${data.strIngredient2}</li>
-  <li>${data.strMeasure3} ${data.strIngredient3}</li>
-  <li>${data.strMeasure4} ${data.strIngredient4}</li>
-  <li>${data.strMeasure5} ${data.strIngredient5}</li>
-  <li>${data.strMeasure6} ${data.strIngredient6}</li>
-  <li>${data.strMeasure7} ${data.strIngredient7}</li>
-  <li>${data.strMeasure8} ${data.strIngredient8}</li>
+  <ul>Ingredients
+  ${data.strMeasure1 ? `<li>${data.strMeasure1} ${data.strIngredient1 ?? ''}</li>` : `` }
+  ${data.strMeasure2 ? `<li>${data.strMeasure2} ${data.strIngredient2 ?? ''}</li>` : `` }
+  ${data.strMeasure3 ? `<li>${data.strMeasure3} ${data.strIngredient3 ?? ''}</li>` : `` }
+  ${data.strMeasure4 ? `<li>${data.strMeasure4} ${data.strIngredient4 ?? ''}</li>` : `` }
+  ${data.strMeasure5 ? `<li>${data.strMeasure5} ${data.strIngredient5 ?? ''}</li>` : `` }
+  ${data.strMeasure6 ? `<li>${data.strMeasure6} ${data.strIngredient6 ?? ''}</li>` : `` }
+  ${data.strMeasure7 ? `<li>${data.strMeasure7} ${data.strIngredient7 ?? ''}</li>` : `` }
+  ${data.strMeasure8 ? `<li>${data.strMeasure8} ${data.strIngredient8 ?? ''}</li>` : `` }
+  ${data.strMeasure9 ? `<li>${data.strMeasure9} ${data.strIngredient9 ?? ''}</li>` : `` }
+  ${data.strMeasure10 ? `<li>${data.strMeasure10} ${data.strIngredient10 ?? ''}</li>` : `` }
+  ${data.strMeasure11 ? `<li>${data.strMeasure11} ${data.strIngredient11 ?? ''}</li>` : `` }
+  ${data.strMeasure12 ? `<li>${data.strMeasure12} ${data.strIngredient12 ?? ''}</li>` : `` }
+  ${data.strMeasure13 ? `<li>${data.strMeasure13} ${data.strIngredient13 ?? ''}</li>` : `` }
+  ${data.strMeasure14 ? `<li>${data.strMeasure14} ${data.strIngredient14 ?? ''}</li>` : `` }
+  ${data.strMeasure15 ? `<li>${data.strMeasure15} ${data.strIngredient15 ?? ''}</li>` : `` }
   </ul>
   <p>How to make: ${data.strInstructions}</p>
   `
@@ -41,4 +49,9 @@ function removeCocktail() {
     while (cocktailContainer.lastChild) {
       cocktailContainer.removeChild(cocktailContainer.lastChild)
     }
-  }
+}
+{/* <li>${data.strMeasure5 ?? ''} ${data.strIngredient5 ?? ''}</li>
+<li>${data.strMeasure6 ?? ''} ${data.strIngredient6 ?? ''}</li>
+<li>${data.strMeasure7 ?? ''} ${data.strIngredient7 ?? ''}</li>
+<li>${data.strMeasure8 ?? ''} ${data.strIngredient8 ?? ''}</li>
+</ul> */}
