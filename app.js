f8556736189697ee9b030082a4a126e2d20fd52c
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-06-05 16:21:27 -0700

    Styling

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 async function fetchData() {
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
   try {
-    removeCocktail()
-    let response = await axios.get(url)
-    let data = response.data.drinks[0]
-    showCocktailData(data)
-    console.log(data)
+    removeCocktail();
+    let response = await axios.get(url);
+    let data = response.data.drinks[0];
+    showCocktailData(data);
+    // console.log(data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
 function showCocktailData(data) {
-  const cocktailData = document.querySelector('#cocktail-data')
+  const cocktailData = document.querySelector("#cocktail-data");
   const cocktailInfo = `
   <h2>${data.strDrink}</h2>
   <img src="${data.strDrinkThumb}" alt="drink"/>
-  <ul>Ingredients:
+  <ul>
   ${data.strMeasure1 ? `<li>${data.strMeasure1} ${data.strIngredient1 ?? ''}</li>` : `` }
   ${data.strMeasure2 ? `<li>${data.strMeasure2} ${data.strIngredient2 ?? ''}</li>` : `` }
   ${data.strMeasure3 ? `<li>${data.strMeasure3} ${data.strIngredient3 ?? ''}</li>` : `` }
@@ -33,25 +33,20 @@ function showCocktailData(data) {
   ${data.strMeasure14 ? `<li>${data.strMeasure14} ${data.strIngredient14 ?? ''}</li>` : `` }
   ${data.strMeasure15 ? `<li>${data.strMeasure15} ${data.strIngredient15 ?? ''}</li>` : `` }
   </ul>
-  <p>How to make: ${data.strInstructions}</p>
-  `
-  cocktailData.insertAdjacentHTML("beforeend", cocktailInfo)
+  <p><strong>How to make: </strong> ${data.strInstructions}</p>
+  `;
+  cocktailData.insertAdjacentHTML("beforeend", cocktailInfo);
 }
 
-const button = document.querySelector('button')
-button.addEventListener('click', (e) => {
-  e.preventDefault()
-  fetchData()
-})
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetchData();
+});
 
 function removeCocktail() {
-  const cocktailContainer = document.querySelector('#cocktail-data')
-    while (cocktailContainer.lastChild) {
-      cocktailContainer.removeChild(cocktailContainer.lastChild)
-    }
+  const cocktailContainer = document.querySelector("#cocktail-data");
+  while (cocktailContainer.lastChild) {
+    cocktailContainer.removeChild(cocktailContainer.lastChild);
+  }
 }
-{/* <li>${data.strMeasure5 ?? ''} ${data.strIngredient5 ?? ''}</li>
-<li>${data.strMeasure6 ?? ''} ${data.strIngredient6 ?? ''}</li>
-<li>${data.strMeasure7 ?? ''} ${data.strIngredient7 ?? ''}</li>
-<li>${data.strMeasure8 ?? ''} ${data.strIngredient8 ?? ''}</li>
-</ul> */}
